70275935d8994d110bcb03ddebcc6671e7a42e76
Jean-Phi <jpulpiano@yahoo.it>
2016-05-24 15:56:02 +0200

    Added Qt Support and performance information

--- TraceDebug.cpp
+++ TraceDebug.cpp
@@ -4,7 +4,172 @@
 //
 // //////////////////////////////////////////////////////////////////////
 
+#ifdef ENABLE_TRACE_DEBUG
 #include "TraceDebug.hpp"
-int DebugTrace::deep = 0;
+int DebugTrace::debugPrintDeepness = 0;
 bool DebugTrace::traceActive = true;
 std::map<std::string, int> DebugTrace::mapFileNameToLine;
+std::map<std::string,
+             std::vector<std::pair<std::string,
+                                   std::chrono::system_clock::time_point>>>
+                          DebugTrace::mapFileNameFunctionNameToVectorTimingInfo;
+std::string DebugTrace::GetUniqueKey(const std::string &string1,
+                                     const std::string &string2,
+                                     const std::string &string3) {
+  return string1 + string2 + string3;
+}
+
+DebugTrace::DebugTrace(const std::string& functionName,
+                       const std::string& fileName,
+                       int lineNumber,
+                       const std::string& uniqueKey)
+{
+  keyDebugPerformanceToErase = uniqueKey;
+  debugPerformanceMustBeDisplayed = true;
+  ++debugPrintDeepness;
+  lineHeader = DebugTrace::getSpaces() + functionName + " (" + uniqueKey + ") " +
+      fileName + ":" + std::to_string(lineNumber);
+  AddTrace(std::chrono::steady_clock::now(), "Start measure");
+}
+
+DebugTrace::DebugTrace(const std::string &functionName,
+                       const std::string &fileName,
+                       int lineNumber): debugPrintMustBeDecremented(false)  {
+  debugPerformanceMustBeDisplayed = false;
+  std::string uniqueKey = GetUniqueKey(fileName, functionName);
+  bool savedFileName = mapFileNameToLine.find(uniqueKey) != mapFileNameToLine.end();
+  if(!savedFileName || mapFileNameToLine[uniqueKey] == lineNumber) {
+    keyDebugPrintToErase = uniqueKey;
+    mapFileNameToLine[uniqueKey] = lineNumber;
+    ++debugPrintDeepness;
+    debugPrintMustBeDecremented = true;
+  }
+}
+
+DebugTrace::~DebugTrace() {
+  if((debugPrintMustBeDecremented || debugPerformanceMustBeDisplayed) &&
+     debugPrintDeepness > 0) {
+    --debugPrintDeepness;
+    if(debugPrintMustBeDecremented) {
+      mapFileNameToLine.erase(keyDebugPrintToErase);
+    }
+  }
+  if(debugPrintDeepness == 0) {
+    mapFileNameToLine.clear();
+  }
+  if(debugPerformanceMustBeDisplayed) {
+    AddTrace(std::chrono::steady_clock::now(), "End measure");
+#ifdef USE_QT_DEBUG
+      qDebug() << QString::fromUtf8(GetPerformanceResults().c_str());
+#else
+    std::cout << GetPerformanceResults() << std::endl;
+#endif
+    mapFileNameFunctionNameToVectorTimingInfo.erase(keyDebugPerformanceToErase);
+  }
+}
+
+void DebugTrace::AddTrace(std::chrono::system_clock::time_point timePoint,
+                          const std::string & variableName) {
+  std::pair<std::string, std::chrono::system_clock::time_point> tmpPair =
+      std::make_pair(variableName, timePoint);
+
+  auto vectorTimingInfoIt =
+      mapFileNameFunctionNameToVectorTimingInfo.find(keyDebugPerformanceToErase);
+  if(vectorTimingInfoIt == mapFileNameFunctionNameToVectorTimingInfo.end()) {
+    std::vector<std::pair<std::string,
+                          std::chrono::system_clock::time_point>> tmpVector;
+    tmpVector.push_back(tmpPair);
+    mapFileNameFunctionNameToVectorTimingInfo[keyDebugPerformanceToErase] = tmpVector;
+  } else {
+    vectorTimingInfoIt->second.push_back(tmpPair);
+  }
+
+}
+
+void DebugTrace::PrintPerformanceInfos()
+{
+  std::string output = "";
+
+}
+
+void DebugTrace::ActiveTrace(bool activate) {
+  traceActive = activate;
+}
+
+bool DebugTrace::IsTraceActive() {
+  return traceActive;
+}
+
+std::string DebugTrace::getSpaces() {
+  if(debugPrintDeepness > 1) {
+    return std::string(2 * (debugPrintDeepness - 1), ' ');
+  }
+  return "";
+}
+
+void DebugTrace::PrintString(const std::string& inStr, bool showHierarchy) {
+  std::string str;
+  if(showHierarchy) {
+    str = DebugTrace::getSpaces() + inStr;
+  } else {
+    str = inStr;
+  }
+#ifdef USE_QT_DEBUG
+  qDebug() << QString::fromUtf8(str.c_str());
+#else
+  std::cout << str << std::endl;
+#endif
+}
+
+std::string DebugTrace::GetPerformanceResults() {
+  static auto firstAccess = std::chrono::steady_clock::now();
+  auto now = std::chrono::steady_clock::now();
+  auto performanceInfos = mapFileNameFunctionNameToVectorTimingInfo[keyDebugPerformanceToErase];
+
+  std::string tmp = lineHeader + " (Full elapsed: " +
+      std::to_string(std::chrono::duration <double, std::milli> (now - firstAccess).count()) +
+      std::string(UNIT_TRACE_DEBUG) + ")";
+  auto size = performanceInfos.size() - 1;
+  if(size > 0) {
+    for(decltype(size) index = 0; index < size; ++index)
+    {
+      const auto& valueMin = performanceInfos[index];
+      const auto& valueMax = performanceInfos[index + 1];
+      if(!tmp.empty())
+      {
+        tmp += ", ";
+      }
+      tmp += valueMax.first + " - " + valueMin.first + " = ";
+      tmp +=
+          std::to_string(std::chrono::duration <double, std::milli> (
+                           valueMax.second - valueMin.second).count()) + std::string(UNIT_TRACE_DEBUG);
+    }
+    if(size > 1)
+    {
+      const auto& valueMin = performanceInfos[0];
+      const auto& valueMax = performanceInfos[size];
+      tmp += ", Full time: " + std::to_string(std::chrono::duration <double, std::milli> (
+                           valueMax.second - valueMin.second).count()) + std::string(UNIT_TRACE_DEBUG);
+    }
+  } else {
+    tmp += "Not enough trace to display results.";
+  }
+  return tmp;
+}
+
+//cl /EHsc TraceDebug.cpp
+//#define TRACE_DEBUG_HPP_DEBUG_LOCAL
+#ifdef TRACE_DEBUG_HPP_DEBUG_LOCAL
+void main()
+{
+  START_TRACE_PERFORMANCE(testJP);
+  auto val1 = std::chrono::steady_clock::now();
+  std::cout << "Hello, world!" << std::endl;
+  ADD_TRACE_PERFORMANCE(testJP, testJP1Val1);
+  std::cout << "Hello, world 2!" << std::endl;
+  auto val2 = std::chrono::steady_clock::now();
+  std::cout << "val 2 - val1 = " << std::chrono::duration <double, std::milli> (
+                           val2 - val1).count() << std::endl;
+}
+#endif
+#endif
--- TraceDebug.hpp
+++ TraceDebug.hpp
@@ -1,6 +1,7 @@
 // //////////////////////////////////////////////////////////////////////
 //
 // This file is under the license http://www.apache.org/licenses/LICENSE-2.0
+// Accessible on GitHub: https://github.com/2BlackCoffees/C-DebugClasses
 //
 // //////////////////////////////////////////////////////////////////////
 
@@ -29,75 +30,148 @@
 // ->f1.cpp:83 (f1) f2() = 1
 //->main.cpp:368 (main) f1() = 1
 
+// Analyze of performances
+// -----------------------
+
+// void ClassName::anyFct() {
+//   START_TRACE_PERFORMANCE(my_unique_id);
+//   /* function body */
+// }
+
+// Will produce
+// "ClassName::anyFct (my_unique_id) filename.cpp:362 (Full elapsed: 398.000000 ms), End measure - Start measure = 3.000000 ms"
+
+// Nested functions can be analyzed as well and any trace point can be easily added:
+
+// void ClassName::anyFct() {
+//   START_TRACE_PERFORMANCE(my_unique_id);
+//   for (...) {
+//     START_TRACE_PERFORMANCE(my_unique_id1);
+//     // code here
+//     ADD_TRACE_PERFORMANCE(my_unique_id1, "User information");
+//     // code here
+//     Usage of my_unique_id instead of my_unique_id1 is possible too: many information will be provided on one line
+//   }
+//   /* function body */
+// }
 
 #include <map>
+#include <utility>
 #include <string>
+#include <vector>
 #include <iostream>
+#include <chrono>
+#include <sstream>
 
-#define TOKENPASTE(x, y) x ## y
-#define TOKENPASTE_EXPAND(x, y) TOKENPASTE(x , y)
-// Activate traces
-#define DISPLAY_DEBUG_ACTIVE_TRACE DebugTrace::ActiveTrace(true)
-// Deactivate traces: any call to any debug macro will not be displaying anything. However the hierarchy is kept up to date.
-// When traces are activated back the last computed hierarchy will be used to insert the right number of blank spaces
-#define DISPLAY_DEBUG_DEACTIVE_TRACE DebugTrace::ActiveTrace(false)
-// Display a value specifying the expression, its value and many blank spaces defining the deepness of the hierarchy,
-// the filename, line number and function name are all displayed.
+//#define ENABLE_TRACE_DEBUG
+#ifdef ENABLE_TRACE_DEBUG
+  #ifdef _WIN32
+    #define __func__ __FUNCTION__
+    #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+  #else
+    #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+  #endif
+
+  #ifdef UNIT_TRACE_DEBUG_NANO
+    #define UNIT_TRACE_DEBUG " ns"
+    #define UNIT_TRACE_TEMPLATE_TYPE std::nano
+  #else
+    #define UNIT_TRACE_DEBUG " ms"
+    #define UNIT_TRACE_TEMPLATE_TYPE std::milli
+  #endif
+  #define USE_QT_DEBUG
+  #ifdef USE_QT_DEBUG
+    #include <QDebug>
+  #endif
+  #define TOKENPASTE(x, y) x ## y
+  #define TOKENPASTE_EXPAND(x, y) TOKENPASTE(x , y)
+  // Activate traces
+  #define DISPLAY_DEBUG_ACTIVE_TRACE DebugTrace::ActiveTrace(true)
+  // Deactivate traces: any call to any debug macro will not be displaying anything. However the hierarchy is kept up to date.
+  // When traces are activated back the last computed hierarchy will be used to insert the right number of blank spaces
+  #define DISPLAY_DEBUG_DEACTIVE_TRACE DebugTrace::ActiveTrace(false)
+  // Display a value specifying the expression, its value and many blank spaces defining the deepness of the hierarchy,
+  // the filename, line number and function name are all displayed.
 #define DISPLAY_DEBUG_VALUE(value) \
-  DebugTrace TOKENPASTE_EXPAND(__Unused, __LINE__)(__func__, __FILE__, __LINE__); \
+  DebugTrace TOKENPASTE_EXPAND(__Unused, __LINE__)(__func__, __FILENAME__, __LINE__); \
   if(DebugTrace::IsTraceActive()) { \
-    std::cout << DebugTrace::getSpaces() << "Processing " << #value << "  From " << __BASE_FILE__ << ":" << __LINE__ << " ("<< __func__ << ")" << std::endl; \
-    std::cout << DebugTrace::getSpaces() << "->" << __BASE_FILE__ << ":" << __LINE__ << " ("<< __func__ << ")\t" << #value << " = " << (value) << std::endl; \
+    std::stringstream stream1;\
+    std::stringstream stream2;\
+    stream1 << "Processing " << #value << "  From " << __FILENAME__ << ":" << __LINE__ << " ("<< __func__ << ")"; \
+    DebugTrace::PrintString(stream1.str(), true);\
+    stream2 << "->" << __FILENAME__ << ":" << __LINE__ << " ("<< __func__ << ")  " << #value << " = " << (value); \
+    DebugTrace::PrintString(stream2.str(), true);\
   }
-// Display a message and many blank spaces defining the deepness of the hierarchy
-#define DISPLAY_DEBUG_MESSAGE(message) \
-    DebugTrace TOKENPASTE_EXPAND(__Unused, __LINE__)(__func__, __FILE__, __LINE__); \
-    if(DebugTrace::IsTraceActive()) { std::cout << message << std::endl; }
-// Display a value specifying the expression, its value and many blank spaces defining the deepness of the hierarchy
-// deeper hierarchy will however not be displayed
-#define DISPLAY_DEBUG_VALUE_NON_HIER(value) \
-    DebugTrace TOKENPASTE_EXPAND(__Unused, __LINE__)(__func__, __FILE__, __LINE__); \
-    if(DebugTrace::IsTraceActive()) { \
-      DISPLAY_DEBUG_DEACTIVE_TRACE; \
-      std::cout << DebugTrace::getSpaces() << __BASE_FILE__ << ":" << __LINE__ << " ("<< __func__ << ")\t" << #value << " = " << (value) << std::endl; \
-      DISPLAY_DEBUG_ACTIVE_TRACE; \
-    }
+#define DISPLAY_IMMEDIATE_DEBUG_VALUE(value) \
+  DebugTrace TOKENPASTE_EXPAND(__Unused, __LINE__)(__func__, __FILENAME__, __LINE__); \
+  if(DebugTrace::IsTraceActive()) { \
+    std::stringstream stream;\
+    stream << __FILENAME__ << ":" << __LINE__ << " ("<< __func__ << ")  " << #value << " = " << (value); \
+    DebugTrace::PrintString(stream.str(), true);\
+  }
+  // Display a message and preceeded by blank spaces defining the deepness of the hierarchy
+  #define DISPLAY_DEBUG_MESSAGE(message) \
+      DebugTrace TOKENPASTE_EXPAND(__Unused, __LINE__)(__func__, __FILENAME__, __LINE__); \
+      if(DebugTrace::IsTraceActive()) { DebugTrace::PrintString(message, true); }
+  // Display a value specifying the expression, its value and preceed them with blank spaces defining the deepness of the hierarchy
+  // deeper hierarchy will however not be displayed
+  #define DISPLAY_DEBUG_VALUE_NON_HIERARCHICALLY(value) \
+      DebugTrace TOKENPASTE_EXPAND(__Unused_Debug, __LINE__)(__func__, __FILENAME__, __LINE__); \
+      if(DebugTrace::IsTraceActive()) { \
+        DISPLAY_DEBUG_DEACTIVE_TRACE; \
+        std::stringstream stream;\
+        stream << __BASE_FILE__ << ":" << __LINE__ << " ("<< __func__ << ")  " << #value << " = " << (value) << std::endl; \
+        DebugTrace::PrintString(stream.str(), true);\
+        DISPLAY_DEBUG_ACTIVE_TRACE; \
+      }
+  #define START_TRACE_PERFORMANCE(unique_key) \
+    DebugTrace TOKENPASTE_EXPAND(unique_key, _Performance_Variable)(__func__, __FILENAME__, __LINE__, #unique_key);
+  #define ADD_TRACE_PERFORMANCE(unique_key, userInfo) \
+    auto TOKENPASTE_EXPAND(unique_key, __LINE__) = std::chrono::steady_clock::now();\
+    TOKENPASTE_EXPAND(unique_key, _Performance_Variable).AddTrace(TOKENPASTE_EXPAND(unique_key, __LINE__), userInfo);
 
-class DebugTrace {
-    static int deep;
-    static bool traceActive;
-    static std::map<std::string, int> mapFileNameToLine;
-    bool mustBeDecremented;
-    std::string keyToErase;
-  public:
-    DebugTrace(const std::string & functionName, const std::string & fileName = __FILE__, int lineNumber = __LINE__): mustBeDecremented(false)  {
-      std::string uniqueKey = fileName + functionName;
-      bool savedFileName = mapFileNameToLine.find(uniqueKey) != mapFileNameToLine.end();
-      if(!savedFileName || mapFileNameToLine[uniqueKey] == lineNumber) {
-        keyToErase = uniqueKey;
-        mapFileNameToLine[uniqueKey] = lineNumber;
-        ++deep;
-        mustBeDecremented = true;
-      }
-    }
-    ~DebugTrace() {
-      if(mustBeDecremented && deep > 0) {
-        --deep;
-        mapFileNameToLine.erase(keyToErase);
-      }
-      if(deep == 0) {
-        mapFileNameToLine.clear();
-      }
-    }
-    static void ActiveTrace(bool activate) {
-      traceActive = activate;
-    }
-    static bool IsTraceActive() {
-      return traceActive;
-    }
-    static std::string getSpaces() {
-      return std::string(2 * (deep - 1), ' ');
-    }
-};
 
+  class DebugTrace {
+      static int debugPrintDeepness;
+      static bool traceActive;
+      static std::map<std::string, int> mapFileNameToLine;
+      static std::map<std::string, std::vector<std::pair<std::string,
+                                               std::chrono::system_clock::time_point>>>
+                mapFileNameFunctionNameToVectorTimingInfo;
+      bool debugPrintMustBeDecremented;
+      bool debugPerformanceMustBeDisplayed;
+      std::string keyDebugPrintToErase;
+      std::string keyDebugPerformanceToErase;
+      std::string lineHeader;
+      std::string GetUniqueKey(const std::string & string1,
+                               const std::string & string2,
+                               const std::string & string3 = "");
+
+    public:
+      DebugTrace(const std::string &functionName, const std::string &fileName, int lineNumber, const std::string &uniqueKey);
+
+      DebugTrace(const std::string & functionName,
+                 const std::string & fileName,
+                 int lineNumber = __LINE__);
+      ~DebugTrace();
+      static void PrintPerformanceInfos();
+
+      static void ActiveTrace(bool activate);
+      static bool IsTraceActive();
+      static std::string getSpaces();
+      static void PrintString(const std::string& inStr, bool showHierarchy);
+
+      std::string GetPerformanceResults();
+      void  AddTrace(std::chrono::system_clock::time_point timePoint, const std::string &variableName);
+  };
+#else
+  #define DISPLAY_DEBUG_ACTIVE_TRACE
+  #define DISPLAY_DEBUG_DEACTIVE_TRACE
+  #define DISPLAY_DEBUG_VALUE(value)
+  #define DISPLAY_IMMEDIATE_DEBUG_VALUE(value)
+  #define DISPLAY_DEBUG_MESSAGE(message)
+  #define DISPLAY_DEBUG_VALUE_NON_HIERARCHICALLY(value)
+  #define START_TRACE_PERFORMANCE(unique_key)
+  #define ADD_TRACE_PERFORMANCE(unique_key, userInfo)
+#endif
 #endif
